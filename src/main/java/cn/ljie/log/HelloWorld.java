1420f87263f2741f734b0ad5b0c35c229d3def41
Administrator <tsss2019666@gmail.com>
2022-02-27 16:15:50 +0800

    修改文件5

---
+++ src/main/java/cn/ljie/log/HelloWorld.java
@@ -0,0 +1,7 @@
+package cn.ljie.log;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("111");
+    }
+}
--- src/main/java/cn/ljie/log/TestLog4jsql.java
+++ src/main/java/cn/ljie/log/TestLog4jsql.java
@@ -3,11 +3,12 @@ package cn.ljie.log;
 
 import org.apache.log4j.Logger;
 
-public class TestLog4j {
+public class TestLog4jsql {
     private static Logger logger = Logger.getLogger("log4jdemo");
 
     public static void main(String[] args) {
         for (int i = 0; i < 1000; i++)
             logger.debug("RollingFileAppenderTest:" + i);
     }
+    /*haha*/
 }
